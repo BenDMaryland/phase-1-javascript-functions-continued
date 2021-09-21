09da1eb17d9cb567d8ef9ea289cc2c19b0623fed
Benjamin <ben.d.maryland@gmail.com>
2021-09-20 23:22:41 -0400

    commit tree 545665458d79ef979e222f8d98fc735422892085

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function saturdayFun(plans = 'roller-skate') {
+return `This Saturday, I want to ${plans}!`
+}
+
+
+function mondayWork (monPlan = 'go to the office'){
+    return `This Monday, I will ${monPlan}.`
+}
+
+function wrapAdjective (inner='*'){
+   return function (intt) {
+       
+        return `You are ${inner}${intt}${inner}!`;
+  }
+   }
